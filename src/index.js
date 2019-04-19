b1156990a8763d261036b7c3db1c454a8bbd3c2d
neha chauhan <nehachauhan79@gmail.com>
2019-04-19 18:39:38 +0530

    Added route for user

--- src/index.js
+++ src/index.js
@@ -1,58 +1,24 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
-const taskRouter = require('./routers/task');
 
+// Models
+// const User = require('./models/user');
+// const Task = require('./models/task');
+
+// Router
+const taskRouter = require('./routers/task');
+const userRouter = require('./routers/user');
+
+// Initialise app
 const app = express(),
     port = process.env.PORT || 1800;
 
+// provide resources
 app.use(express.json())
 app.use(taskRouter)
+app.use(userRouter)
 
-// User model APIs
-// Create Request
-app.post('/users', async (req, res) => {
-    
-    const user = new User(req.body)
-    try {
-        const result = await user.save()
-        res.status(201).send(result)
-    } catch(e) {
-        res.status(500).send(e);
-    }
-    
-});
-
-// Read Request 
-
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users);
-    } catch(e) {
-        res.status(500).send(e);
-    }
-});
-
-app.get('/users/:id', async(req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    } catch(e) {
-        res.status(500).send(e)
-    }
-});
-
-// Task model APIs 
-
-
-
+// start app
 app.listen(port, () => {
     console.log(`Server is up and running at ${port}`)
 })
---
+++ src/routers/user.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = new express.Router();
+const User = require('../models/user');
+
+router.post('/users', async (req, res) => {
+    
+    const user = new User(req.body)
+    try {
+        const result = await user.save()
+        res.status(201).send(result)
+    } catch(e) {
+        res.status(500).send(e);
+    }
+    
+});
+
+// Read Request 
+
+router.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users);
+    } catch(e) {
+        res.status(500).send(e);
+    }
+});
+
+router.get('/users/:id', async(req, res) => {
+    const _id = req.params.id;
+    try {
+        const user = await User.findById(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+});
+
+module.exports = router;
